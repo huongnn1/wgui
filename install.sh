4a10b06180a3bb13dbe140283764d22a07c68f1a
huongnn1 <nn.huong1793@gmail.com>
2024-01-10 22:55:15 +0700

    update

--- install.sh
+++ install.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 #Step 1: Update repository linux
+echo "Updating repository..."
 apt update
 
 #Step 2: Install wireguard
+echo "Installing wireguard..."
 apt install wireguard -y
 
 #Step 3: Enable IP forwarding
+echo "Enable IP forwarding..."
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 #Step 4: Git clone source
+echo "Cloing source code..."
 cd /etc/wireguard/
 git clone https://github.com/huongnn1/wgui.git
+echo "Moving source code to /etc/wireguard..."
 mv /etc/wireguard/wgui/* /etc/wireguard/
 
 #Step 5: Copy file to /etc/systemd/system/ then reload daemon
+echo "Copying file services to /etc/sytemd/system/..."
 cp /etc/wireguard/systemd/* /etc/systemd/system/
 systemctl daemon-reload
 
 #Step 6: Update wireguard-ui
+echo "Updating source code wireguard-ui..."
 chmod +x /etc/wireguard/update.sh
 cd /etc/wireguard/
 ./update.sh
