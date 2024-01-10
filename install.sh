a8520c4cb6d2f300853d7c3916de19429a513b54
huongnn1 <nn.huong1793@gmail.com>
2024-01-10 23:00:39 +0700

    update

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ cd /etc/wireguard/
 git clone https://github.com/huongnn1/wgui.git
 echo "Moving source code to /etc/wireguard..."
 mv /etc/wireguard/wgui/* /etc/wireguard/
+chmod +x -R /etc/wireguard/
 
 #Step 5: Copy file to /etc/systemd/system/ then reload daemon
 echo "Copying file services to /etc/sytemd/system/..."
