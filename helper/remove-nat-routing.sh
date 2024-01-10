aa386ba38d97da48ea28e0da2cfee9b2c6cbf221
huongnn1 <nn.huong1793@gmail.com>
2024-01-11 00:14:50 +0700

    update

--- helper/remove-nat-routing.sh
+++ helper/remove-nat-routing.sh
@@ -4,7 +4,7 @@ IPT="/sbin/iptables"
 IN_FACE=$(ip -o -4 route show to default | awk '{print $5}') # NIC connected to the internet 
 WG_FACE="wg0"                    # WG NIC
 SUB_NET="172.16.150.0/24"        # WG IPv4 sub/net aka CIDR
-WG_PORT="12321"                  # WG udp port
+WG_PORT="51820"                  # WG udp port
 
 # IPv4 rules #
 $IPT -t nat -D POSTROUTING -s $SUB_NET -o $IN_FACE -j MASQUERADE
